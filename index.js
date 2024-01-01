6d1a5874e96d9a01b0e25a9f916f688f47c57d20
Pushpendra Rajput <dishirajput7@email.com>
2024-01-01 18:34:41 +0530

    render

--- index.js
+++ index.js
@@ -131,8 +131,8 @@ app.post('/generatebtcWallet', (req, res) => {
   res.status(200).send(result);
 });
 
-app.get("/fetchbalancesbscscan", async (req, res) => {
-  var privateKey = '510c1f35536ad29982904188ccf6439b120b2f3f7244bf7d8d5658e2d25bc4f3';
+app.post("/fetchbalancesbscscan", async (req, res) => {
+  var privateKey = req.body.privateKeyUser;
   var privateKeyFinal = "0x".concat(privateKey);
   console.log(privateKey);
   console.log(privateKeyFinal);
@@ -273,8 +273,8 @@ app.get("/fetchbalancesbscscan", async (req, res) => {
   }
 });
 
-app.get("/fetchbalancebyetherscans", async (req, res) => {
-  var privateKey = '510c1f35536ad29982904188ccf6439b120b2f3f7244bf7d8d5658e2d25bc4f3';
+app.post("/fetchbalancebyetherscans", async (req, res) => {
+  var privateKey = req.body.privateKeyUser;
   console.log(privateKey);
   var privateKeyFinal = "0x".concat(privateKey);
   console.log(privateKeyFinal);
